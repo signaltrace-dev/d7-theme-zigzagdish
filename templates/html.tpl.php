f71cf26105b8d83520a1305207661f193f7c00a8
Jeff Glenn <jeff@signaltrace.net>
2016-07-27 09:53:44 -0400

    Add Google AdWords tracking code

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -2,7 +2,7 @@
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
 <head<?php print $rdf->profile; ?>>
   <?php print $head; ?>
-  <title><?php print $head_title; ?></title>  
+  <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
@@ -15,6 +15,37 @@
 <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=405934022864583&ev=PageView&noscript=1"/></noscript>
 <!-- End Facebook Pixel Code -->
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-54ced64a5d764f66" async="async"></script>
+
+<!-- Google Code for adwords conversion Conversion Page
+In your html page, add the snippet and call
+goog_report_conversion when someone clicks on the
+chosen link or button. -->
+<script type="text/javascript">
+  /* <![CDATA[ */
+  goog_snippet_vars = function() {
+    var w = window;
+    w.google_conversion_id = 955327954;
+    w.google_conversion_label = "xG43CO_ik2gQ0svExwM";
+    w.google_remarketing_only = false;
+  }
+  // DO NOT CHANGE THE CODE BELOW.
+  goog_report_conversion = function(url) {
+    goog_snippet_vars();
+    window.google_conversion_format = "3";
+    var opt = new Object();
+    opt.onload_callback = function() {
+      if (typeof(url) != 'undefined') {
+        window.location = url;
+      }
+    }
+    var conv_handler = window['google_trackConversion'];
+    if (typeof(conv_handler) == 'function') {
+      conv_handler(opt);
+    }
+  }
+  /* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion_async.js"></script>
 
 </head>
 <body<?php print $attributes;?>>
